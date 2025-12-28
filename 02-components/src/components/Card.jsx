8b5483189fba3fd0a80e2e4b56806008dfe6a022
Sujwal <dsujwal24@gmail.com>
2025-12-28 21:20:01 +0545

    02-components done

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Hello World</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+  );
+};
+
+export default Card;
---
+++ 02-components/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="nav">I am a nav</div>
+    </div>
+  );
+};
+
+export default Navbar;
