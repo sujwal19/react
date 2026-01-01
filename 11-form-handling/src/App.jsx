df6c106cd541930403863ef06b4fcd62118a78d3
Sujwal <dsujwal24@gmail.com>
2026-01-01 15:39:12 +0545

    form handling concept grasp

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,21 @@
+const App = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted");
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+      >
+        <input type="text" placeholder="Enter your name" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
