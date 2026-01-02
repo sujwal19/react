2fa28aa6acc1838f813cc8850e9fc1446be0b893
Sujwal <dsujwal24@gmail.com>
2026-01-02 16:53:31 +0545

    localStorage basics

---
+++ 14-localstorage/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  let users = JSON.parse(localStorage.getItem("user"));
+
+  console.log(users);
+
+  return (
+    <div>
+      <h2>Sujwal</h2>
+      <h3></h3>
+    </div>
+  );
+};
+
+export default App;
