82dfd44131b89861b40e85a3bc0b99342fd153fb
Sujwal <dsujwal24@gmail.com>
2026-01-20 12:54:02 +0545

    Accordion

--- project-component/src/App.jsx
+++ project-component/src/App.jsx
@@ -1,48 +1,29 @@
-import { useState } from "react";
-import Button from "./components/Button";
-// import TextDisplay from "./components/TextDisplay";
-// import Counter from "./components/Counter";
+import React, { useState } from "react";
 
-const App = () => {
-  // const [counter, setCounter] = useState(0);
-  // const [isVisible, setIsVisible] = useState(false);
-  // const [input, setInput] = useState("");
+function Accordion() {
+  const [openIndex, setOpenIndex] = useState(null);
 
-  const [fruits, setFruits] = useState(["Apple", "Banana", "Mango"]);
-
-  const [color, setColor] = useState("blue");
-
-  function randomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
-  const handleClick = () => {
-    setColor(randomColor());
-  };
+  const data = [
+    { title: "Section 1", content: "This is section 1 content." },
+    { title: "Section 2", content: "This is section 2 content." },
+    { title: "Section 3", content: "This is section 3 content." },
+  ];
 
   return (
     <div>
-      {/* <Counter /> */}
-      {/* <TextDisplay /> */}
-      {/* <ul>
-        {fruits.map((fruit, idx) => {
-          return <li key={idx}>{fruit}</li>;
-        })}
-      </ul> */}
-      <div
-        className="m-20 flex h-100 w-100 items-end justify-center text-xl"
-        style={{ backgroundColor: color }}
-        onClick={handleClick}
-      >
-        {color}
-      </div>
+      {data.map((item, idx) => (
+        <div key={idx} className="text-2xl">
+          <h2
+            className="border-2 bg-cyan-500"
+            onClick={() => setOpenIndex(openIndex == idx ? null : idx)}
+          >
+            {item.title}
+          </h2>
+          {openIndex == idx && <p>{item.content}</p>}
+        </div>
+      ))}
     </div>
   );
-};
+}
 
-export default App;
+export default Accordion;
