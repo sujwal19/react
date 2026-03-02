783ef6e2685f1cfa7f52ae817f20f532159153fe
Sujwal <dsujwal24@gmail.com>
2026-03-02 17:45:08 +0545

    Doing Small Projects

---
+++ small-projects/src/MealAPI/Meal.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import "./meal.css";
+import axios from "axios";
+
+const Meal = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(async () => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        setItems(res.data.meals);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const itemsList = items.map(({ strMeal, strMealThumb, idMeal }) => {
+    return (
+      <section className="card">
+        <img src={strMealThumb} alt={strMeal} />
+
+        <section className="content">
+          <p>{strMeal}</p>
+          <p>#{idMeal}</p>
+        </section>
+      </section>
+    );
+  });
+
+  return <div className="items-container">{itemsList}</div>;
+};
+
+export default Meal;
