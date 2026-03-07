95e3d5bb81fc7bb8820511d3a1cc00e4a3b207d6
Sujwal <dsujwal24@gmail.com>
2026-03-07 20:46:13 +0545

    Expense Tracker in Progress

--- expense-tracker-mern/server.js
+++ expense-tracker-mern/server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
 const app = express();
+app.use(express.json());
 const transactions = require("./routes/transactions");
 
 app.use("/api/v1/transactions", transactions);
@@ -13,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`.bgBlue
-      .white.bold,
+    `Server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold,
   ),
 );
